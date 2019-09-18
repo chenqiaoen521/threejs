0bcc8e3146c8fae687183d5e6f7998a5f80829ba
chenqiaoen521 <274078815@qq.com>
2019-09-18 17:01:55 +0800

    add

--- js/main.js
+++ js/main.js
@@ -14,11 +14,12 @@ function init() {
   // add the lights
   createLights();
    // add the objects
-  //createPlane();
+  createPlane();
   createSea();
   createSky();
   // start a loop that will update the objects' positions 
   // and render the scene on each frame
+  document.addEventListener('mousemove', handleMouseMove, false);
   loop();
 }
 
@@ -160,9 +161,112 @@ function createSky() {
   scene.add(sky.mesh);
 }
 
+var AirPlane = function () {
+  this.mesh = new THREE.Object3D();
+  // create the cabin
+  var geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1);
+  var matCockpit = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
+
+  geomCockpit.vertices[4].y-=10;
+  geomCockpit.vertices[4].z+=20;
+  geomCockpit.vertices[5].y-=10;
+  geomCockpit.vertices[5].z-=20;
+  geomCockpit.vertices[6].y+=30;
+  geomCockpit.vertices[6].z+=20;
+  geomCockpit.vertices[7].y+=30;
+  geomCockpit.vertices[7].z-=20;
+
+  var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
+  cockpit.castShadow = true;
+  cockpit.receiveShadow = true;
+  this.mesh.add(cockpit);
+
+  // create the engine
+  var geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1);
+  var matEngine = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
+  var engine = new THREE.Mesh(geomEngine, matEngine);
+  engine.position.x = 40;
+  engine.castShadow = true;
+  engine.receiveShadow = true;
+  this.mesh.add(engine);
+     
+  // Create the tail
+  var geomTailPlane = new THREE.BoxGeometry(15,20,5,1,1,1);
+  var matTailPlane = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
+  var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
+  tailPlane.position.set(-35,25,0);
+  tailPlane.castShadow = true;
+  tailPlane.receiveShadow = true;
+  this.mesh.add(tailPlane);
+   
+  // Create the wing
+  var geomSideWing = new THREE.BoxGeometry(40,8,150,1,1,1);
+  var matSideWing = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
+  var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
+  sideWing.castShadow = true;
+  sideWing.receiveShadow = true;
+  this.mesh.add(sideWing);
+   
+  // propeller
+  var geomPropeller = new THREE.BoxGeometry(20,10,10,1,1,1);
+  var matPropeller = new THREE.MeshPhongMaterial({color:Colors.brown, shading:THREE.FlatShading});
+  this.propeller = new THREE.Mesh(geomPropeller, matPropeller);
+  this.propeller.castShadow = true;
+  this.propeller.receiveShadow = true;
+   
+  // blades
+  var geomBlade = new THREE.BoxGeometry(1,100,20,1,1,1);
+  var matBlade = new THREE.MeshPhongMaterial({color:Colors.brownDark, shading:THREE.FlatShading});
+   
+  var blade = new THREE.Mesh(geomBlade, matBlade);
+  blade.position.set(8,0,0);
+  blade.castShadow = true;
+  blade.receiveShadow = true;
+  this.propeller.add(blade);
+  this.propeller.position.set(50,0,0);
+  this.mesh.add(this.propeller);
+
+}
+
+var airplane;
+  function createPlane() {
+    airplane = new AirPlane();
+    airplane.mesh.scale.set(.25,.25,.25);
+    airplane.mesh.position.y = 100;
+    scene.add(airplane.mesh);
+  }
+
 function loop () {
   renderer.render(scene, camera);
   sea.mesh.rotation.z += .005;
   sky.mesh.rotation.z += .01;
+  updatePlane();
   requestAnimationFrame(loop);
+}
+
+var mousePos = {x: 0, y: 0};
+
+function handleMouseMove(event) {
+  var tx = -1 + (event.clientX / WIDTH) * 2;
+  var ty = 1 - (event.clientY / HEIGHT) * 2;
+  mousePos = {x: tx, y: ty};
+}
+
+function updatePlane() {
+  var targetX = normalize(mousePos.x, -1, 1, -100, 100);
+  var targetY = normalize(mousePos.y, -1, 1, 25, 175);
+  airplane.mesh.position.y = targetY;
+  airplane.mesh.position.x = targetX;
+  airplane.propeller.rotation.x += 0.3;
+}
+
+function normalize(v,vmin,vmax,tmin, tmax){
+ 
+  var nv = Math.max(Math.min(v,vmax), vmin);
+  var dv = vmax-vmin;
+  var pc = (nv-vmin)/dv;
+  var dt = tmax-tmin;
+  var tv = tmin + (pc*dt);
+  return tv;
+ 
 }
